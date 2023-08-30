9def6375a1b9c2f22dd7791c1faa7231d5981b95
CarlosHdz7 <carloshdz.udb@gmail.com>
2023-08-30 14:37:53 -0600

    Delete discord-ui-v2 directory

--- discord-ui-v2/src/components/Divider/Divider.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Divider = () => {
-  return (
-    <hr className="border dark:border-gray-100 border-gray-500  rounded-full my-[10px]" />
-  );
-};
-
-export default Divider;
